ed06456620781f470fdf34945e3d3dff1f027c09
[JECSTER] <[jorcasce@gmail.com]>
2022-11-16 15:54:43 +0000

    Commit made from VS Code Azure Static Web Apps

---
+++ api/message/index.js
@@ -0,0 +1,7 @@
+module.exports = async function (context, req) {
+    module.exports = async function (context, req) {
+        context.res.json({
+            text: "Hello from the JECSAPI"
+        });
+    };
+}
